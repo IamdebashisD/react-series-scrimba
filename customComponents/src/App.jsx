e677398b67db771db20b0148e1cc9fe4a6043952
IamdebashisD <adebashisdas626@gmail.com>
2025-07-10 20:18:55 +0530

    added some custom components

--- customComponents/src/App.jsx
+++ customComponents/src/App.jsx
@@ -1,24 +1,15 @@
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function CustomComponent() {
   return (
     <>
-      <header>
-        <img src={reactLogo} alt="react-logo" width="40px" />
-      </header>
-      <main>
-        <h1>Reasons, I'm exited to learn React</h1>
-
-        <ol>
-          <li>React is exciting to learn because it is a popular, powerful, and versatile JavaScript library for building user interfaces</li>
-          <li>It's known for its ease of use, component-based architecture, and strong community support, making it a great choice for both beginners and experienced developers</li>
-          <li> Furthermore, React's popularity leads to numerous job opportunities with competitive salaries. </li>
-        </ol>
-      </main>
-      <footer>
-        <small>Copyright &copy; das development. All rights reserved.</small>
-      </footer>
+      <Header Logo={reactLogo}/>
+      <MainContent />
+      <Footer />
     </>
   )
 }
